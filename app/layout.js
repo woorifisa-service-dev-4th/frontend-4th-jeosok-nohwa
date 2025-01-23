d9421e2596ef3ce69ebf37d7dda479658887cef5
HyeYoung-Hwang <vkfvks1004@naver.com>
2025-01-23 18:30:16 +0900

    feat: header부분 겹치지 않게 layout에 pt적용

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CommonHeader />
-        {children}
+        <div className="pt-16">{children}</div>
       </body>
     </html>
   );
