320cb8aec16b170f95698da6446ee7a24eb38caa
taegung <yesongmin12@gamil.com>
2025-01-23 15:11:13 +0900

    feat :모달 컴포넌트 구현

---
+++ app/components/ui/modal/Modal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image"; // Next.js Image 컴포넌트 추가
+import "./modal.css"; // 추가 스타일 가져오기
+
+const Modal = ({ isOpen, onClose, children, date }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        {/* 헤더 */}
+        <div className="modal-header">
+          <div className="modal-logo-container">
+            <Image
+              src="/image/jeosok-nohwa-logo.png"
+              alt="logo"
+              width={40} // 버튼 크기와 동일한 너비
+              height={40} // 버튼 크기와 동일한 높이
+              className="rounded-full"
+            />
+          </div>
+          <span className="modal-date">{date}</span>
+          <button
+            onClick={() => {
+              onClose(); // 모달 닫기 실행
+            }}
+            className="modal-close"
+          >
+            ×
+          </button>
+        </div>
+
+        {/* 콘텐츠 */}
+        <div className="modal-content">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
