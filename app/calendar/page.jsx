ad0178298a05327490c15adb4edfe214e2239f99
june0216 <ziyun1612@ewhain.net>
2025-01-24 11:33:22 +0900

    feat : 홈으로 이동하는 기능 추가

---
+++ app/calendar/page.jsx
@@ -0,0 +1,11 @@
+import MyCalendar from '../components/MyCalendar';
+import NavBar from "../components/nav-bar";
+
+export default function Page() {
+  return (
+    <div>
+      <MyCalendar/>
+      <NavBar />
+    </div>
+  );
+}
