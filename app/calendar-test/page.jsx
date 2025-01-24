d7915c1a6cb746030e85488617e1375e43d6c3b1
yunsy1103 <yunsy1103@naver.com>
2025-01-24 14:14:58 +0900

    feat: 캘린더 모달 컴포넌트 생성

---
+++ app/calendar-test/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useState } from "react";
+import CalendarModal from "@/app/components/CalendarModal";
+
+const CalendarPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태
+  const [selectedDate, setSelectedDate] = useState(null); // 선택된 날짜
+
+  const openModal = () => setIsModalOpen(true); // 모달 열기
+  const closeModal = () => setIsModalOpen(false); // 모달 닫기
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date); // 선택한 날짜 업데이트
+    closeModal(); // 모달 닫기
+
+    // 페이지 이동 (예: 날짜별 페이지로 이동)
+    const formattedDate = date.toISOString().split("T")[0]; // YYYY-MM-DD 형식
+    router.push(`/chat/${formattedDate}`); // /date/2025-01-01 과 같은 경로로 이동
+  };
+
+  return (
+    <div>
+      <h1>캘린더 모달 테스트</h1>
+      <p>
+        선택된 날짜: {selectedDate ? selectedDate.toDateString() : "없음"}
+      </p>
+      <button onClick={openModal}>날짜 선택</button>
+
+      {/* CalendarModal 컴포넌트 */}
+      <CalendarModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onDateChange={handleDateChange}
+      />
+    </div>
+  );
+};
+
+export default CalendarPage;
---
+++ app/components/CalendarModal.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import dynamic from "next/dynamic";
+import "react-calendar/dist/Calendar.css";
+import '../styles/custom-calendar.css';
+import "../components/ui/modal/modal.css"; 
+import '../styles/custom-calendar-modal.css';
+import MyCalendar from "./MyCalendar";
+
+// React-Calendar를 동적으로 로드 (SSR 방지)
+const Calendar = dynamic(() => import("react-calendar"), { ssr: false });
+
+const CalendarModal = ({ isOpen, onClose, selectedDate, onDateChange }) => {
+  if (!isOpen) return null;
+
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains("modal-overlay")) {
+      onClose(); // 모달 닫기 함수 호출
+    }
+  };
+  
+  // 날짜 클릭 이벤트 핸들러
+  const handleDateClick = (date) => {
+    const formattedDate = date.toLocaleDateString('ko-KR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    }).replace(/\. /g, '-').replace('.', '').trim(); // "yyyy.mm.dd" 형식을 "yyyy-mm-dd"로 변환
+    router.push(`/chat/${formattedDate}`);
+  };
+  
+
+  return (
+    <div className="modal-overlay" onClick={handleOverlayClick}>
+      <div className="modal-container">
+        {/* React Calendar */}
+        <div className="modal-content">
+          <MyCalendar 
+          view="days" 
+          minDetail="month" // 세기 뷰를 비활성화
+          maxDetail="month"
+          onClickDay={handleDateClick}
+          onChange={onDateChange} 
+          value={selectedDate} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarModal;
