837c17b2c3c65b8805afe5ac526ee46f35eae4e8
june0216 <ziyun1612@ewhain.net>
2025-01-28 03:15:24 +0900

    fix : 높이를 계산하여 동적으로 고정

---
+++ components/ui/ClientViewportHandler.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ClientViewportHandler() {
+    useEffect(() => {
+        function setViewportHeight() {
+            document.documentElement.style.setProperty(
+                "--vh",
+                `${window.innerHeight * 0.01}px`
+            );
+        }
+
+        setViewportHeight(); // 초기 실행
+        window.addEventListener("resize", setViewportHeight); // 리사이즈 감지
+
+        return () => {
+            window.removeEventListener("resize", setViewportHeight); // 이벤트 제거
+        };
+    }, []);
+
+    return null; // UI를 렌더링하지 않음
+}
