0fe540274db7b1aa848d085f8bb89a01c6b0c133
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2025-01-31 16:45:58 +0900

    feat : main 페이지 UI 구현

---
+++ components/main/RouterButton.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function RouterButton() {
+  const router = useRouter();
+
+  return (
+    <>
+      <button
+        className="bg-[#eaf7e7] text-[#3b3b3b] text-[16px] font-[400] w-full py-3 rounded-lg mt-9 shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),_0px_1px_1px_0px_rgba(0,0,0,0.17)]"
+        onClick={() => router.push("/home")}
+      >
+        시작하기
+      </button>
+      <button
+        className="bg-[#ffffff] text-[#909090] text-[16px] font-[400] w-full py-3 rounded-lg mt-5 shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),_0px_1px_1px_0px_rgba(0,0,0,0.17)]"
+        onClick={() => router.push("/home")}
+      >
+        저속노화 알아보기
+      </button>
+    </>
+  );
+}
