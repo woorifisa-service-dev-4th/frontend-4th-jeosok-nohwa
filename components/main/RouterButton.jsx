be15b5da82aa65a6ae41b9d9f331c4e6c069eb6f
june0216 <ziyun1612@ewhain.net>
2025-02-02 19:01:53 +0900

    fix : 색상 수정

--- components/main/RouterButton.jsx
+++ components/main/RouterButton.jsx
@@ -9,13 +9,13 @@ export default function RouterButton() {
   return (
     <>
       <button
-        className="bg-[#eaf7e7] text-[#3b3b3b] text-[16px] font-[400] w-full py-3 rounded-lg mt-9 shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),_0px_1px_1px_0px_rgba(0,0,0,0.17)]"
+        className="bg-mainGreen text-[#3b3b3b] text-[16px] font-[400] w-full py-3 rounded-lg mt-9 shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),_0px_1px_1px_0px_rgba(0,0,0,0.17)]"
         onClick={() => router.push("/home")}
       >
         시작하기
       </button>
       <button
-        className="bg-[#ffffff] text-[#909090] text-[16px] font-[400] w-full py-3 rounded-lg mt-5 shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),_0px_1px_1px_0px_rgba(0,0,0,0.17)]"
+        className="bg-[#ffffff] text-mainGray text-[16px] font-[400] w-full py-3 rounded-lg mt-5 shadow-[0px_0px_2px_0px_rgba(0,0,0,0.08),_0px_1px_1px_0px_rgba(0,0,0,0.17)]"
         onClick={() => router.push("/home")}
       >
         저속노화 알아보기
