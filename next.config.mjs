4768738253b7b6afd5a63b53e302fa473181d1c7
june0216 <ziyun1612@ewhain.net>
2025-02-02 17:03:18 +0900

    fix : css 최적화 off

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true, // Vercel 배포 중 ESLint 오류 무시
     },
+    experimental: {
+        optimizeCss: false, // CSS 최적화
+    }
 };
 
 export default nextConfig;
